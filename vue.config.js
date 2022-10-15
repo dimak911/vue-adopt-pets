42c3023d1f1cd0265143a5d50a75f59de3fa84d8
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-10-15 18:46:19 +0300

    add public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/vue-adopt-pet/",
+  publicPath: "/vue-adopt-pets/",
 });
